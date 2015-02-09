323dc0910f60750ef393de0506c04496acaa69a1
Alex Davis <alex2006hw@gmail.com>
2015-02-09 15:25:57 -0800

    updated

--- sodimm/.chef/knife.rb
+++ sodimm/.chef/knife.rb
@@ -3,10 +3,10 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "wixproducts"
-client_key               "#{current_dir}/wixproducts.pem"
-validation_client_name   "wixproducts-validator"
-validation_key           "#{current_dir}/wixproducts-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/wixproducts"
+node_name                "sodimm"
+client_key               "#{current_dir}/sodimm.pem"
+validation_client_name   "sodimm-validator"
+validation_key           "#{current_dir}/sodimm-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/sodimm"
 syntax_check_cache_path  "#{ENV['HOME']}/.chef/syntaxcache"
 cookbook_path            ["#{current_dir}/cookbooks"]
